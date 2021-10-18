c775a849ce4b6002b247fed0bf9d2eb10ff4421d
sreekanth1619 <87491860+sreekanth1619@users.noreply.github.com>
2021-10-18 12:15:58 +0530

    Added: "IAM in sad mood" messgae should be return in sad

---
+++ moodanalyser/src/main/java/com/bridgelabs/moodanalyser/MoodAnalyser.java
@@ -0,0 +1,14 @@
+package com.bridgelabs.moodanalyser;
+/**
+ * Purpose  - “I am in Sad Mood” message Should Return SAD
+ * @author  - N Sreekanth
+ * @version - 16.0
+ *
+ */
+public class MoodAnalyser {
+	 public static String analyseMood(String message) {
+	        if (message.toLowerCase().contains("User")) {
+	        }
+	        return "sree";
+	    }
+}
