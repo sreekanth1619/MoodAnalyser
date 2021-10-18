23d2662aa1349b82e6dcb0036790d3e20e0e6705
sreekanth1619 <87491860+sreekanth1619@users.noreply.github.com>
2021-10-18 12:45:13 +0530

    Added: given null mood should return happy

--- moodanalyser/src/main/java/com/bridgelabs/moodanalyser/MoodAnalyser.java
+++ moodanalyser/src/main/java/com/bridgelabs/moodanalyser/MoodAnalyser.java
@@ -6,11 +6,33 @@ package com.bridgelabs.moodanalyser;
  *
  */
 public class MoodAnalyser {
-	public static String analyseMood(String message) {
-        if (message.toLowerCase().contains("sad")) {
-            return "Sad";
-        } else if (message.toLowerCase().contains("Any")) {
-        }
-        return "Happy";
-    }
+	  public static String message;
+
+	    // Constructor
+	    public MoodAnalyser(String message) {
+	        this.message = message;
+	        analyseMood();
+	    }
+
+	    //Getter
+	    public String getMessage() {
+	        return message;
+	    }
+
+	    //Setter
+	    public void setMessage(String message) {
+	        this.message = message;
+	    }
+
+	    public static String analyseMood() {
+	        try {
+	            if (message.toLowerCase().contains("sad")) {
+	                return "Sad";
+	            } else {
+	                return "Happy";
+	            }
+	        } catch (NullPointerException e) {
+	            return "Happy";
+	        }
+	    }
 }
--- moodanalyser/src/test/java/com/bridgelabs/moodanalyser/MoodAnalyserTest.java
+++ moodanalyser/src/test/java/com/bridgelabs/moodanalyser/MoodAnalyserTest.java
@@ -3,18 +3,24 @@ import org.junit.Test;
 import org.junit.Assert;
 
 public class MoodAnalyserTest {
-	// Test case to analyze sad mood of the user
-    @Test
+	@Test
     public void givenMessage_WhenProper_ShouldReturnSad() {
-        MoodAnalyser moodanalyser=new MoodAnalyser();
-        String actualResult=moodanalyser.analyseMood("User is Sad");
-        Assert.assertEquals("Sad",actualResult);
+        MoodAnalyser moodanalyser = new MoodAnalyser("I am in Sad Mood");
+        String actualResult = moodanalyser.analyseMood();
+        Assert.assertEquals("Sad", actualResult);
     }
-    // Test case to analyze happy mood of the user
+
     @Test
-    public void givenMessage_ContainsAny_init_ShouldReturnHappy() {
-        MoodAnalyser moodanalyser = new MoodAnalyser();
-        String actualResult = moodanalyser.analyseMood("I am in Any Mood");
+    public void givenMessage_WhenProper_ShouldReturnHappy() {
+        MoodAnalyser moodanalyser = new MoodAnalyser("I am in happy Mood");
+        String actualResult = moodanalyser.analyseMood();
+        Assert.assertEquals("Happy", actualResult);
+    }
+
+    @Test
+    public void givenMessage_Null_ShouldReturnHappy() {
+        MoodAnalyser moodanalyser = new MoodAnalyser(null);
+        String actualResult = moodanalyser.analyseMood();
         Assert.assertEquals("Happy", actualResult);
     }
 }
