6be3e2d002b102c634901ad305d0d16992a438a9
sreekanth1619 <87491860+sreekanth1619@users.noreply.github.com>
2021-10-18 12:40:02 +0530

    Added: "Iam in any mood" message should be return happy

--- moodanalyser/src/test/java/com/bridgelabs/moodanalyser/MoodAnalyserTest.java
+++ moodanalyser/src/test/java/com/bridgelabs/moodanalyser/MoodAnalyserTest.java
@@ -8,6 +8,13 @@ public class MoodAnalyserTest {
     public void givenMessage_WhenProper_ShouldReturnSad() {
         MoodAnalyser moodanalyser=new MoodAnalyser();
         String actualResult=moodanalyser.analyseMood("User is Sad");
-        Assert.assertEquals("sree",actualResult);
+        Assert.assertEquals("Sad",actualResult);
+    }
+    // Test case to analyze happy mood of the user
+    @Test
+    public void givenMessage_ContainsAny_init_ShouldReturnHappy() {
+        MoodAnalyser moodanalyser = new MoodAnalyser();
+        String actualResult = moodanalyser.analyseMood("I am in Any Mood");
+        Assert.assertEquals("Happy", actualResult);
     }
 }
