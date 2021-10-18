3b36203ef67fe623449be9ada5c55de8bcd3084c
sreekanth1619 <87491860+sreekanth1619@users.noreply.github.com>
2021-10-18 12:15:32 +0530

    Added: "IAM in sad mood" messgae should be return in sad

---
+++ moodanalyser/src/test/java/com/bridgelabs/moodanalyser/MoodAnalyserTest.java
@@ -0,0 +1,13 @@
+package com.bridgelabs.moodanalyser;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class MoodAnalyserTest {
+	// Test case to analyze sad mood of the user
+    @Test
+    public void givenMessage_WhenProper_ShouldReturnSad() {
+        MoodAnalyser moodanalyser=new MoodAnalyser();
+        String actualResult=moodanalyser.analyseMood("User is Sad");
+        Assert.assertEquals("sree",actualResult);
+    }
+}
