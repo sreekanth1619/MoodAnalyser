1ca9ac4dec46799f9f24a368c4219cff6f7d3926
sreekanth1619 <87491860+sreekanth1619@users.noreply.github.com>
2021-10-18 13:37:13 +0530

    Added: Given null Mood Should Throw MoodAnalysisException

---
+++ moodanalyser/src/main/java/com/bridgelabs/moodanalyser/MoodAnalysisException.java
@@ -0,0 +1,19 @@
+package com.bridgelabs.moodanalyser;
+
+public class MoodAnalysisException extends Exception {
+    public String message;
+
+    public MoodAnalysisException(String message) {
+        super(message);
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
